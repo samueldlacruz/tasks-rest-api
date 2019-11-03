80c6e45d65cbf01edcb93f505b96a66dc2af1c00
samuel <samueldlacg@gmail.com>
2019-11-03 00:08:20 -0500

    test

--- src/entries/database/index.js
+++ src/entries/database/index.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 const { db } = require('../../../config/index');
 
+//connected db mongo
 mongoose.connect(db, {
     useCreateIndex: true,
     useNewUrlParser: true,
